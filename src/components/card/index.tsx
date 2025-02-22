d5c5b45b5e955189862352bb6f68940da14f4391
Roger Jacob <mail@rogerjacob.com>
2025-02-22 09:26:50 +0400

    feat: open news URL in new tab on card click

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -11,14 +11,16 @@ const Card = (props: CardProps) => {
 
   return (
     <div className={styles.card}>
-      <div>
-        <img className={styles.cardImage} src={data?.thumbnail} />
-      </div>
-      <div className={styles.cardContent}>
-        <h2 className={styles.cardTitle}>{formatTitle(data?.title, 60)}</h2>
-        <h5 className={styles.cardDate}>{data?.date}</h5>
-        {data?.category && <span className={styles.cardTag}>{data?.category}</span>}
-      </div>
+      <a href={data?.url} target="_blank">
+        <div>
+          <img className={styles.cardImage} src={data?.thumbnail} />
+        </div>
+        <div className={styles.cardContent}>
+          <h2 className={styles.cardTitle}>{formatTitle(data?.title, 60)}</h2>
+          <h5 className={styles.cardDate}>{data?.date}</h5>
+          {data?.category && <span className={styles.cardTag}>{data?.category}</span>}
+        </div>
+      </a>
     </div>
   );
 };
